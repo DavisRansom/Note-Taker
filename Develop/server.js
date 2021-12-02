7a9e771bc07076db232463c4a10a7b8264b3e608
Davis Ransom <davisransom.vb@gmail.com>
2021-12-01 21:47:33 -0800

    Added unfinished API Routing code and README template

--- Develop/server.js
+++ Develop/server.js
@@ -6,36 +6,93 @@ const port = 3000;
 
 
 //Static File Routing
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //HTML Routing
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./public/index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-app.get("/notes",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./public/notes.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //API Routing
-app.get('/api/notes',(req,res) =>{
-  //from the front end getNotes function  
+app.get('/api/notes', (req, res) => {
+    //from the front end getNotes function  
+    try {
+        // reads the notes from json file
+        notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+        console.log("hello!");
+        // parse it so notesData is an array of objects
+        notesData = JSON.parse(notesData);
+
+        // error handling
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    //   send objects to the browser
+    res.json(notesData);
 });
-app.post('/api/notes',(req,res) =>{
-//from the saveNote function from the front end
-});
-app.delete('/api/notes/:id',(req,res) =>{
+
+app.post('/api/notes', (req, res) => {
+    //from the saveNote function from the front end
+    {
+        try {
+            // reads the json file
+            notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+            console.log(notesData);
+            // parse the data to get an array of objects
+            notesData = JSON.parse(notesData);
+            // Set new notes id
+            req.body.id = notesData.length;
+            // add the new note to the array of note objects
+            notesData.push(req.body); // req.body - user input
+            // make it string(stringify)so you can write it to the file
+            notesData = JSON.stringify(notesData);
+            // writes the new note to file
+            fs.writeFile("./Develop/db/db.json", notesData, "utf8", function (err) {
+                // error handling
+                if (err) throw err;
+            });
+            // change it back to an array of objects & send it back to the browser(client)
+            res.json(JSON.parse(notesData));
+
+            // error handling
+        } catch (err) {
+            throw err;
+            console.error(err);
+        }
+    };
+app.delete('/api/notes/:id', (req, res) => {
     //from the deleteNote function from the front end
+    try {
+        //  reads the json file
+        notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+        // parse the data to get an array of objects
+        notesData = JSON.parse(notesData);
+        // delete the old note from the array on note objects
+        notesData = notesData.filter(function (note) {
+            return note.id != req.params.id;
+        });
+        // make it into a string so I can write it to the file
+        notesData = JSON.stringify(notesData);
+        // write the new notes to the file
+        fs.writeFile("./Develop/db/db.json", notesData, "utf8", function (err) {
+            // error handling
+            if (err) throw err;
+        });
+
+        // change it back to an array of objects & send it back to the browser
+        res.send(JSON.parse(notesData));
+
+        // error handling
+    } catch (err) {
+        throw err;
+        console.log(err);
+    }
 });
 
-
-
-
-
-
-
-
-
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Server is running on port", port);
 });
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Note-Taker
+A simple note taking app that allows users to add, recall, or delete notes.
+
+# Description
+An in-depth paragraph about your project and overview of use.
+
+# Getting Started
+Dependencies
+Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+ex. Windows 10
+Installing
+How/where to download your program
+Any modifications needed to be made to files/folders
+Executing program
+How to run the program
+Step-by-step bullets
+code blocks for commands
+Help
+Any advise for common problems or issues.
+
+
+# Authors
+Davis Ransom
+https://github.com/DavisRansom
+davis.ransom.tech@gmail.com
+
+# Version History
+This is the first version of this application.
+
+# License
+This project is licensed under the [MIT] License
+
+# Acknowledgments
+Thank you to Coach Will for his support and guidance on this project.
